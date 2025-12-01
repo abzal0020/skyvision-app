0ee01494273604a078da2c265558fd61e865b9eb
abzal0020 <abzalkojaixan3@gmail.com>
2025-12-01 12:38:53 +0500

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -6,9 +6,9 @@ function Contact() {
       <h1>Контакты</h1>
       <p>Свяжитесь с нами по следующим каналам:</p>
       <ul style={{ fontSize: "18px", lineHeight: "1.8" }}>
-        <li><strong>Телеграм:</strong> @MeiWenti_logistics</li>
-        <li><strong>Email:</strong> info@MeiWenti.kz</li>
-        <li><strong>WhatsApp:</strong> +7 777 000 0000</li>
+        <li><strong>Телеграм:</strong> @SKYVISION_logistics</li>
+        <li><strong>Email:</strong> info@SKYVISION.kz</li>
+        <li><strong>WhatsApp:</strong> +7 747 165 4092</li>
       </ul>
     </div>
   );
