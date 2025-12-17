d3a118f70b7e1ee1396631d217b8511ee9754241
abzal0020 <abzalkojaixan3@gmail.com>
2025-12-17 11:43:27 +0500

    Update WhatsApp contact number in Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
       <ul style={{ fontSize: "18px", lineHeight: "1.8" }}>
         <li><strong>Телеграм:</strong> @SKYVISION_logistics</li>
         <li><strong>Email:</strong> info@SKYVISION.kz</li>
-        <li><strong>WhatsApp:</strong> +7 747 165 4092</li>
+        <li><strong>WhatsApp:</strong> +7 771 525 2683</li>
       </ul>
     </div>
   );
